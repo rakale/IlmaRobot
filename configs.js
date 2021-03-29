03a2d5b10ef41908b6f1037a7ff962c5f7c20b32
Raiko Kalev <rakale@ttu.ee>
2021-03-29 13:26:20 +0300

    first commit

---
+++ configs.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // twitter api configs
+  consumer_key: "ZNIn6J7GgBQ3CS41pk4W5ps4u",
+  consumer_secret: "ZEvshlCDSbNTdIsrZlGPSt2hpbwLgHcX3Z3gMkL7MBYVrzLNXk",
+  access_token: "1376442387321470982-SzrF2hwVvYBFB9n5N7owan4ntnlFte",
+  access_token_secret: "4z0wo5Xjbn5Zw2HdT03rUCwzV0mTFisx1qQ1QLjPzv3QB",
+
+  apiLink: "http://api.openweathermap.org/data/2.5/weather?q=",
+  city: "Tallinn",
+  appID: [(appid = "72c567c7141a3a3f528ae6ac678c5c59")],
+  units: [(units = "metric")],
+};
---
+++ ilmaRobot.js
@@ -0,0 +1,75 @@
+var request = require("request");
+var Twit = require("twit");
+const { apiLink } = require("./configs");
+var configs = require("./configs");
+
+var TwitterApi = new Twit(configs);
+var currentTemp;
+var minimumTemp;
+var maximumTemp;
+var weatherDescription;
+var windSpeed;
+var url = "http://api.openweathermap.org/data/2.5/weather?q=Tallinn&APPID=72c567c7141a3a3f528ae6ac678c5c59&units=metric"
+
+  function GetData() {
+      console.log(url)
+    request({ url: url, json: true }, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        currentTemp = body.main.temp;
+        minimumTemp = body.main.temp_min;
+        maximumTemp = body.main.temp_max;
+        humidity = body.main.humidity;
+        windSpeed = body.wind.speed;
+        weatherDescription = body.weather[0].description;
+        CleanData();
+      } else {
+        console.log("Error triggered inside of the GetData function.");
+        console.log(error);
+      }
+    });
+  }
+
+  function CleanData() {
+    currentTemp = currentTemp.toFixed(0);
+    minimumTemp = minimumTemp.toFixed(0);
+    maximumTemp = maximumTemp.toFixed(0);
+    windSpeed = windSpeed.toFixed(0);
+    Tweet();
+  }
+
+  function Tweet() {
+    var weatherUpdate =
+      "Currently " +
+      configs.city +
+      " is experiencing " +
+      weatherDescription +
+      " at " +
+      currentTemp +
+      "°C." +
+      "\n" +
+      "Daily forecast: min of " +
+      minimumTemp +
+      "°C " +
+      "& a max of " +
+      maximumTemp +
+      "°C with " +
+      humidity +
+      "% Humidity" +
+      " & " +
+      windSpeed +
+      "m/s Wind. \n#Tallinn";
+    var tweet = {
+      status: weatherUpdate,
+    };
+    TwitterApi.post("statuses/update", tweet, callback);
+    function callback(error) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Tweeted successfully: \n" + weatherUpdate);
+      }
+    }
+  }
+
+  setInterval(GetData, 1000 * 60 * 60);
+  
